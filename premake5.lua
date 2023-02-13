3071b06127f1d144e28a3aa54d556bd252febb47
GigalightGuy <80604755+GigalightGuy@users.noreply.github.com>
2023-02-13 00:43:42 +0000

    Removed Dist configuration option from premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -79,7 +79,3 @@ project "GLFW"
     filter "configurations:Release"
         runtime "Release"
         optimize "on"
-
-    filter "configurations:Dist"
-        runtime "Release"
-        optimize "on"
